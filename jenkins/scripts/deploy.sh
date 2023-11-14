1d4bd3d98f75aae9c79d3e8b6baff9ed3191e8a7
lhs64 <2100594@sit.singaporetech.edu.sg>
2023-11-14 21:15:43 +0800

    commitsion

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,10 +1,10 @@
-    #!/usr/bin/env sh
+#!/usr/bin/env sh
 
-    set -x
-    docker run -d -p 80:80 --name my-apache-php-app -v $(pwd)/src:/var/www/htmlsrc:/var/www/html php:7.2-apache
-    sleep 1
-    set +x
+set -x
+docker run -d -p 80:80 --name my-apache-php-app -v c:/Users/Steven Goh/Documents/GitHub/test/Lab07B/src:/var/www/html php:7.2-apache
+sleep 1
+set +x
 
-    echo 'Now...'
-    echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Now...'
+echo 'Visit http://localhost to see your PHP application in action.'
 
